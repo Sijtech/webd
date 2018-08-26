d796365f32b111670be2ba6187fe8eab19d5d452
sijtech <joesiewerdt@gmail.com>
2018-08-26 20:06:43 +0200

    optimized

--- templates/include/footer.php
+++ templates/include/footer.php
@@ -1,8 +1,7 @@
-      <div id="footer">
-        <h1>Footer</h1>
-        Widget News &copy; 2011. All rights reserved. <a href="admin.php">Site Admin</a>
-      </div>
-
-    </div>
+    	</article>
+    </section>
+    <footer>
+      Widget News &copy; 2011. All rights reserved. <a href="admin.php">Site Admin</a>
+    </footer>
   </body>
 </html>
