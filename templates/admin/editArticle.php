cde3eef3df7bfee6fb20190df11c7ebc0cc7334d
sijtech <joesiewerdt@gmail.com>
2018-08-26 19:48:54 +0200

    dev

--- templates/admin/editArticle.php
+++ templates/admin/editArticle.php
@@ -1,10 +1,9 @@
-<?php include "templates/include/header.php";
-    echo $results['article']['id'];
-?>
+<?php include "templates/include/header.php"; ?>
 <div id="art">
   <form action="admin.php?action=<?php echo $results['formAction'] ?>" method="post">
     <input type="hidden" name="idArtikel" value="<?php echo $results['article']->id ?>" />
     <input type="hidden" name="Autor" value="1"/>
+    <input type="hidden" name="Erstelldatum" id="erstelldatum" placeholder="YYYY-MM-DD HH:mm:ss" value="<?php echo $results['article']->erstelldatum ?>" />
     <ul>
       <li>
         <label for="title">Titel</label>
@@ -15,11 +14,6 @@
         <label for="content">Artikel Inhalt</label>
         <textarea name="Artikel" id="content" placeholder="Artikel" required maxlength="100000" style="height: 30em;"><?php echo $results['article']->artikel ?></textarea>
       </li>
-
-      <!-- <li>
-        <label for="erstellungsdatum">Erstellungsdatum</label>
-        <input disabled type="date" name="Erstelldatum" id="erstellungsdatum" placeholder="YYYY-MM-DD HH:mm:ss" required maxlength="10" value="<?php echo $results['article']->erstelldatum ?>" />
-      </li>-->
     </ul>
 
     <div class="buttons">
@@ -28,6 +22,8 @@
     </div>
 
   </form>
-
+    <?php if (!is_null($results['article']->id)) { ?>
+      <p><a href="admin.php?action=deleteArticle&amp;id=<?php echo $results['article']->id ?>" onclick="return confirm('Diesen Artikel löschen?')">Artikel löschen</a></p>
+    <?php } ?>
 </div>
 <?php include "templates/include/footer.php" ?>
